3db53279f0ab21d3716499f25711b2ece947830b
xoterin <44329085+xoterin@users.noreply.github.com>
2018-10-20 19:55:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+#include "Deck.h"
+#include "Card.h"
+
+/* Passes in a vector Cards and returns true if any 2 Cards have the same rank.
+   Otherwise returns false.
+*/
+bool hasPair(const vector<Card> &);
+
+
+/* Passes in a vector of Cards and outputs to the stream each Card
+   separated by a comma and a space, 
+   with no comma, space, or newline at the end.
+*/
+ostream & operator<<(ostream &, const vector<Card> &);
